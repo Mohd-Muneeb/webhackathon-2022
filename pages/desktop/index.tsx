8874a724ed47be2960018a92c8e20809799b3d3a
Mohd-Muneeb <mohdmuneeb1307@gmail.com>
2022-11-30 15:28:25 +0530

    Small changes

--- pages/desktop/index.tsx
+++ pages/desktop/index.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { Breadcrumb } from "../../components/Breadcrumb";
 import Card from "../../components/Card";
+import Loading from "../../components/Loading";
 
-const Desktop = () => {
-	return (
-		<div className="w-[90vw] ml-[5vw] mt-12">
-			<div className="w-[80vw] ml-[5vw]">
-				<Breadcrumb />
-				{/* <div className="w-[70vw] ml-[5vw] h-full my-8 p-4 border-solid border-2 border-yellow-500 rounded-2xl bg-[hsl(var(--wa))] text-[hsl(var(--n))]">
-					<h1>
-						</h1>Some warning here
-				</div> */}
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-			</div>
-		</div>
-	);
+const Desktop = (props: any) => {
+	if (props.loading) {
+		return <Loading />;
+	} else {
+		if (props.user) {
+			return (
+				<div className="w-[90vw] ml-[5vw] mt-12">
+					<div className="w-[80vw] ml-[5vw]">
+						<Breadcrumb />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+					</div>
+				</div>
+			);
+		} else {
+			window.location.href = "/login"
+		}
+	}
 };
 
 export default Desktop;
--- pages/mobile/index.tsx
+++ pages/mobile/index.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { Breadcrumb } from "../../components/Breadcrumb";
 import Card from "../../components/Card";
+import Loading from "../../components/Loading";
 
-const Mobile = () => {
-	return (
-		<div className="w-[90vw] ml-[5vw] mt-12">
-			<div className="w-[80vw] ml-[5vw]">
-				<Breadcrumb />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-			</div>
-		</div>
-	);
+const Mobile = (props: any) => {
+	if (props.loading) {
+		return <Loading />;
+	} else {
+		if (props.user) {
+			return (
+				<div className="w-[90vw] ml-[5vw] mt-12">
+					<div className="w-[80vw] ml-[5vw]">
+						<Breadcrumb />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+					</div>
+				</div>
+			);
+		} else {
+			window.location.href = "/login";
+		}
+	}
 };
 
 export default Mobile;
