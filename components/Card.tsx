1fde2294db1651fa31723c66c68553989d76d658
Mohd-Muneeb <mohdmuneeb1307@gmail.com>
2022-11-30 16:18:35 +0530

    Database connection

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,30 +1,35 @@
 import React from "react";
 
-const Card = () => {
-	return (
-		<div className="p-6 rounded-xl bg-base-200 my-4 flex flex-col md:flex-row gap-8">
-			<img src="https://placeimg.com/400/225/arch" alt="" className="rounded-xl" />
-			<div className="h-full flex flex-col my-4 gap-12">
-				<div>
-					<h1 className="text-2xl font-semibold text-[hsl(var(--p))]">Title</h1>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi voluptatibus perferendis quidem
-						earum accusamus unde harum repudiandae? Qui consequatur facilis, assumenda, temporibus omnis
-						quae at ipsum facere quidem illum incidunt.
-					</p>
-					<p className="text-[hsl(var(--s))]">Total downloads: </p>
-				</div>
-				<div className="bottom-0 flex flex-col md:flex-row gap-4">
-					<a href="https://placeimg.com/400/225/arch" download>
-						<button className="btn btn-primary">Download</button>
-					</a>
-					<a href="https://placeimg.com/400/225/arch" target="_blank" rel="noreferrer">
-						<button className="btn btn-primary btn-outline">View Source</button>
-					</a>
+const Card = (props: any) => {
+	console.log(props);
+	if (props.title === undefined) {
+		return <></>;
+	} else {
+		return (
+			<div className="p-6 rounded-xl bg-base-200 my-4 flex flex-col md:flex-row gap-8">
+				<img src={props.image} alt="" className="rounded-xl max-h-80" />
+				<div className="h-full flex flex-col my-4 gap-12">
+					<div>
+						<h1 className="text-2xl font-semibold text-[hsl(var(--p))]">{props.title}</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi voluptatibus perferendis
+							quidem earum accusamus unde harum repudiandae? Qui consequatur facilis, assumenda,
+							temporibus omnis quae at ipsum facere quidem illum incidunt.
+						</p>
+						<p className="text-[hsl(var(--s))]">Total downloads: {props.downloads}</p>
+					</div>
+					<div className="bottom-0 flex flex-col md:flex-row gap-4">
+						<a href={props.image} download>
+							<button className="btn btn-primary">Download</button>
+						</a>
+						<a href={props.image} target="_blank" rel="noreferrer">
+							<button className="btn btn-primary btn-outline">View Source</button>
+						</a>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export default Card;
--- pages/desktop/index.tsx
+++ pages/desktop/index.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Breadcrumb } from "../../components/Breadcrumb";
 import Card from "../../components/Card";
+import { collection, query, getDocs } from "firebase/firestore";
 import Loading from "../../components/Loading";
+import { db } from "../../db/firebase";
 
 const Desktop = (props: any) => {
+	const [Wallpapers, setWallpapers] = useState([{}]);
+	let key = 0;
+	const [firstLoad, setfirstLoad] = useState(false);
+	useEffect(() => {
+		if (!firstLoad) {
+			setfirstLoad(true);
+		} else {
+			const getPosts = async () => {
+				const q = query(collection(db, "desktop"));
+
+				const querySnapshot = await getDocs(q);
+				querySnapshot.forEach((doc) => {
+					setWallpapers((prev) => [...prev, doc.data()]);
+				});
+			};
+			getPosts();
+		}
+	}, [firstLoad]);
+
 	if (props.loading) {
 		return <Loading />;
 	} else {
@@ -12,15 +33,15 @@ const Desktop = (props: any) => {
 				<div className="w-[90vw] ml-[5vw] mt-12">
 					<div className="w-[80vw] ml-[5vw]">
 						<Breadcrumb />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
+						{Wallpapers.map((elem: any) => {
+							console.log(elem);
+							return <Card downloads={elem.downloads} title={elem.title} image={elem.image} key={elem.title} />
+						})}
 					</div>
 				</div>
 			);
 		} else {
-			window.location.href = "/login"
+			window.location.href = "/login";
 		}
 	}
 };
