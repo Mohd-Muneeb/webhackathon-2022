428c6470e87ddd55b6a896b5ed59569139c8a574
Mohd-Muneeb <mohdmuneeb1307@gmail.com>
2022-12-01 16:47:49 +0530

    Adding small change

--- pages/mobile/index.tsx
+++ pages/mobile/index.tsx
@@ -1,9 +1,59 @@
-import React from "react";
+// import React from "react";
+// import { Breadcrumb } from "../../components/Breadcrumb";
+// import Card from "../../components/Card";
+// import Loading from "../../components/Loading";
+
+// const Mobile = (props: any) => {
+// 	if (props.loading) {
+// 		return <Loading />;
+// 	} else {
+// 		if (props.user) {
+// 			return (
+// 				<div className="w-[90vw] ml-[5vw] mt-12">
+// 					<div className="w-[80vw] ml-[5vw]">
+// 						<Breadcrumb />
+// 						<Card />
+// 						<Card />
+// 						<Card />
+// 						<Card />
+// 					</div>
+// 				</div>
+// 			);
+// 		} else {
+// 			window.location.href = "/login";
+// 		}
+// 	}
+// };
+
+// export default Mobile;
+
+import React, { useEffect, useState } from "react";
 import { Breadcrumb } from "../../components/Breadcrumb";
 import Card from "../../components/Card";
+import { collection, query, getDocs } from "firebase/firestore";
 import Loading from "../../components/Loading";
+import { db } from "../../db/firebase";
 
-const Mobile = (props: any) => {
+const Desktop = (props: any) => {
+	const [Wallpapers, setWallpapers] = useState([{}]);
+	let key = 0;
+	const [firstLoad, setfirstLoad] = useState(false);
+	useEffect(() => {
+		if (!firstLoad) {
+			setfirstLoad(true);
+		} else {
+			const getPosts = async () => {
+				const q = query(collection(db, "mobile"));
+
+				const querySnapshot = await getDocs(q);
+				querySnapshot.forEach((doc) => {
+					setWallpapers((prev) => [...prev, doc.data()]);
+				});
+			};
+			getPosts();
+		}
+	}, [firstLoad]);
+
 	if (props.loading) {
 		return <Loading />;
 	} else {
@@ -12,10 +62,17 @@ const Mobile = (props: any) => {
 				<div className="w-[90vw] ml-[5vw] mt-12">
 					<div className="w-[80vw] ml-[5vw]">
 						<Breadcrumb />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
+						{Wallpapers.map((elem: any) => {
+							console.log(elem);
+							return (
+								<Card
+									downloads={elem.downloads}
+									title={elem.title}
+									image={elem.image}
+									key={elem.title}
+								/>
+							);
+						})}
 					</div>
 				</div>
 			);
@@ -25,4 +82,4 @@ const Mobile = (props: any) => {
 	}
 };
 
-export default Mobile;
+export default Desktop;
